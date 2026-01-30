57f5d97c197381dacfce68e91f6a4c1ab2fa0962
Praveen Kumar <praveen160405@gmail.com>
2026-01-30 13:18:31 +0000

    same error

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-// IMPORTANT: This file contains your project's Firebase configuration.
-export const firebaseConfig = {
-  apiKey: "AIzaSyAA7tZTnalUv4OCAsU_wMe6WGlsMdSMiEU",
-  authDomain: "studio-8728271286-596e7.firebaseapp.com",
-  projectId: "studio-8728271286-596e7",
-  storageBucket: "studio-8728271286-596e7.appspot.com",
-  messagingSenderId: "881712555776",
-  appId: "1:881712555776:web:3fe086790944534dbfb5e5",
-};
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,15 @@
 import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAA7tZTnalUv4OCAsU_wMe6WGlsMdSMiEU",
+  authDomain: "studio-8728271286-596e7.firebaseapp.com",
+  projectId: "studio-8728271286-596e7",
+  storageBucket: "studio-8728271286-596e7.appspot.com",
+  messagingSenderId: "881712555776",
+  appId: "1:881712555776:web:3fe086790944534dbfb5e5",
+};
 
 export function initializeFirebase() {
   const apps = getApps();
